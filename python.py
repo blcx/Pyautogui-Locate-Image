e3b2db5312dfe61b94ac86a7ed23fca9602398aa
coderx <36203292+blcx@users.noreply.github.com>
2021-01-11 14:23:37 +0530

    Update python.py

--- python.py
+++ python.py
@@ -26,9 +26,11 @@ while 1:
     if pyautogui.locateOnScreen('ball.png', confidence=0.9) != None:
         print("i see")
         image = pyautogui.locateOnScreen('ball.png', confidence=0.8)
-        x, y = pyautogui.center(image)
-        pyautogui.moveTo(x,y)
-        time.sleep(0.1)
+        
+        if image != None:
+            x, y = pyautogui.center(image)
+            pyautogui.moveTo(x,y)
+            time.sleep(0.1)
     else:
         print("i cant see")
         time.sleep(0.5)
